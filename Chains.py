663e0764298ec02adeba40e4d51e2ea9d1a7fcd1
Scott <sadams@mezmeriz.com>
2019-08-21 17:26:00 -0400

    adding visualization for a chain

--- Chains.py
+++ Chains.py
@@ -80,6 +80,102 @@ class Chains:
         o3d.visualization.draw_geometries_with_key_callbacks(pcd, key_to_callback)
 
 
+def makeStraight(superPoints):
+    """
+    Go through each chain.
+    Start one link in if possible
+    go forward until
+    :param superPoints:
+    :return:
+    """
+    dfKeys = [i for i in superPoints.df.keys()]
+    print(dfKeys)
+    CHAIN = dfKeys.index('chain')
+    HEAD = dfKeys.index('head')
+
+    straightSegments = []
+    chains = superPoints.df['chain'].unique()
+    inspectChain = -1000
+    for chain in chains:
+
+
+        df = superPoints.df[superPoints.df['chain']==chain]
+        addedSegment = False
+
+        # Process chain
+        start = df[df['tailFlag']]
+        if len(start) > 0:
+            dfPrint = df[['chain', 'head', 'tail', 'count', 'tailFlag', 'headFlag']]
+            print(dfPrint.head(50))
+            startIndex = start.index.tolist()[0]
+            print("Chain {}: ".format(chain), end="")
+            next = start[start.index == startIndex]
+            headNormalStart = headNormal(df, start)
+            inchWormHead = start
+            inchWormSegments = 0
+            thresholdStraightAngleDeg = 40
+            radii = []
+            infiniteLoop = set([])
+
+            while (next is not None and headNormalStart is not None and next.at[next.index[0], 'chain'] == chain and
+                   next.index[0] not in infiniteLoop):
+
+                try:
+                    print("{}, ".format(next.index[0]), end="")
+                    previous = next
+                    next = df.loc[[next.at[next.index[0], 'head']]]
+                    headNormalNext = headNormal(df, next)
+                    if (np.abs(headNormalNext.dot(headNormalStart)) > np.cos(thresholdStraightAngleDeg * np.pi/180)):
+                        infiniteLoop = infiniteLoop.union(set([previous.index[0]]))
+                        inchWormHead = next
+                        inchWormSegments += 1
+                        radius = (next.at[next.index[0], 'bx'] + next.at[next.index[0], 'by'])/4.0 * 1/448.
+                        radii.append(radius)
+                        print("i", end = "")
+                    else:
+                        if inchWormSegments > 2:
+                            straightSegments.append([chain, start.index[0], inchWormHead.index[0], np.median(radii)])
+                            addedSegment = True
+                        infiniteLoop = infiniteLoop.union(set([previous.index[0]]))
+                        start = next
+                        headNormalStart = headNormal(df, start)
+                        inchWormHead = start
+                        inchWormSegments = 0
+                        radii = []
+
+                        # print(np.abs(headNormalNext.dot(headNormalStart)))
+                        # print(headNormalStart)
+                        # print(headNormalNext)
+                        # print()
+
+                except:
+                    next = None
+                    if inchWormSegments > 2:
+                        straightSegments.append([chain, start.index[0], previous.at[previous.index[0], 'head'], np.median(radii)])
+                        addedSegment = True
+                # head = next.at[next.index[0], 'head']
+                # nextIndex = head
+                # next = df.loc[[head]]
+
+                #
+            print("{}, {}".format(infiniteLoop, addedSegment))
+    print("Segments created {}".format(len(straightSegments)))
+    return straightSegments
+
+def headNormal(df, start):
+    p1 = start.at[start.index[0], 'xyz']
+    head = start.at[start.index[0], 'head']
+
+    if head != -1 and head in df.index.tolist():
+        p2 = df.at[head, 'xyz']
+        # print("P1 {}, P2 {}".format(p1, p2))
+        n = p2 - p1
+        n = n/np.linalg.norm(n)
+        return n
+    else:
+        return None
+
+
 def load(fileName):
     print("Loading {}".format(fileName))
     df = pd.read_pickle(fileName)
